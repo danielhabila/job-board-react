75e02bfa0814497858710d2530d93477a382ed2e
danielhabila <100402238+danielhabila@users.noreply.github.com>
2023-02-14 09:44:23 -0800

    payment has to be successful for job to be created

--- client/src/pages/PostAJob.jsx
+++ client/src/pages/PostAJob.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import UploadImage from "../images/upload.jpg";
 import Footer from "../partials/Footer";
 import Header from "../partials/Header";
 import axios from "axios";
-import Pricing from "../partials/Pricing";
+// import Pricing from "../partials/Pricing";
 
-function PostAJob() {
+function PostAJob(props) {
   const [selectedOption, setSelectedOption] = useState("exact");
   //salary option radio (either exact or range)
   const handleSalaryOptionChange = (option) => {
@@ -58,6 +57,54 @@ function PostAJob() {
     contactEmail: "",
     applyURL: "",
   });
+  // -----------------------------------------------------------------------------------------START
+  const [stick, setStick] = useState(false);
+  const [stickOneWeek, setStickOneWeek] = useState(false);
+  const [highlight, setHighlight] = useState(false);
+
+  const [selectedAddOns, setSelectedAddOns] = useState([]);
+  const [totalCost, setTotalCost] = useState(499);
+
+  const handleAddOnSelection = (addOn) => {
+    if (selectedAddOns.includes(addOn)) {
+      setSelectedAddOns(selectedAddOns.filter((item) => item !== addOn));
+    } else {
+      setSelectedAddOns([...selectedAddOns, addOn]);
+    }
+  };
+  // -----------------------------------------------------------------------------------------START
+  // const checkout = async (event) => {
+  //   try {
+  //     // event.preventDefault();
+  //     console.log(totalCost);
+  //     const response = await axios.post("http://localhost:4000/checkout", {
+  //       totalCost,
+  //       quantity: 1,
+  //     });
+  //     if (response.data.url) {
+  //       window.location.assign(response.data.url); // Forwarding user to Stripe
+  //       return true; // return true if checkout was successful
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //     return false; // return false if checkout was not successful
+  //   }
+  // };
+
+  useEffect(() => {
+    let addOnCost = 0;
+    selectedAddOns.forEach((addOn) => {
+      if (addOn === "24 hour stick") {
+        addOnCost += 99;
+      } else if (addOn === "1 week stick") {
+        addOnCost += 199;
+      } else if (addOn === "highlight") {
+        addOnCost += 49;
+      }
+    });
+    setTotalCost(499 + addOnCost);
+  }, [selectedAddOns]);
+  //-----------------------------------------------------------------------------------------END
 
   const handleChange = (event) => {
     if (
@@ -87,12 +134,11 @@ function PostAJob() {
       });
     }
   };
-
+  //-----------------------------------------------------------------------------------------END
   const handleSubmit = async (event) => {
     event.preventDefault();
+
     try {
-      // const paymentResult = await checkout(event); // Trigger the payment process
-      // if (paymentResult) {
       const formDataWithFile = new FormData();
       formDataWithFile.append("jobTitle", formData.jobTitle);
       formDataWithFile.append("companyName", formData.companyName);
@@ -110,26 +156,33 @@ function PostAJob() {
       if (file) {
         formDataWithFile.append("companyLogo", file, file.name);
       }
+      formDataWithFile.append("totalCost", totalCost);
 
       const response = await axios.post("/CreateJob", formDataWithFile, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-      // }
-      if (response.status === 201) {
-        alert("Job posted successfully!");
-        // const inputFields = document.querySelectorAll(
-        //   "input[type='text'],input[type='number'], textarea"
-        // );
-        // inputFields.forEach((inputField) => (inputField.value = ""));
-      } else {
-        throw new Error(response.status);
+
+      if (response.data.url) {
+        window.location.assign(response.data.url); // Forwarding user to Stripe
       }
     } catch (error) {
       console.log(error.message);
     }
   };
+
+  // stripe.checkout.sessions
+  //   .retrieve(sessionId)
+  //   .then(function (session) {
+  //     session.on("checkout.session.completed", function (event) {
+  //       console.log("Payment successful: ", event);
+  //       alert("Payment successful! Thank you for your purchase.");
+  //     });
+  //   })
+  //   .catch(function (error) {
+  //     console.log("Error retrieving session: ", error);
+  //   });
 
   return (
     <main>
@@ -156,10 +209,7 @@ function PostAJob() {
               </div>
 
               {/* Form */}
-              <form
-                className="mb-12"
-                // onSubmit={handleSubmit}
-              >
+              <form className="mb-12" onSubmit={handleSubmit}>
                 <div className="divide-y divide-gray-200 -my-6">
                   {/* Group #1 */}
                   <div className="py-6">
@@ -446,7 +496,162 @@ function PostAJob() {
                   </div>
 
                   {/* Group #3 *****************************************************/}
-                  <Pricing />
+                  <div className="py-6">
+                    <div className="text-lg font-bold text-gray-800 mb-5">
+                      <span className="text-myred">3.</span> Select add-ons and
+                      pay
+                    </div>
+                    <div className="space-y-4">
+                      {/* Add-on button #1 *****************************************************/}
+                      <button
+                        className={`w-full text-left py-3 px-4 border rounded ${
+                          stick
+                            ? "border-green-300 ring-2 ring-green-300"
+                            : " border-red-200 ring-1 ring-red-200"
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setStick(!stick);
+                          handleAddOnSelection("24 hour stick");
+                        }}
+                      >
+                        <div className="flex justify-between items-center">
+                          <div>
+                            <div className="text-sm text-gray-800 font-medium mb-1">
+                              Stick your post to stay on top for 24 hours (+$99)
+                            </div>
+                            <div className="text-sm text-gray-500 italic">
+                              4x more views
+                            </div>
+                          </div>
+                          <div className="shrink-0 rounded-full border border-gray-200 ml-3">
+                            {stick ? (
+                              <svg
+                                x-show="checked"
+                                className="fill-teal-500"
+                                width="32"
+                                height="32"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="m20.28 12.28-6.292 6.294-2.293-2.293a1 1 0 0 0-1.414 1.414l3 3a1 1 0 0 0 1.414 0l7-7a1 1 0 0 0-1.414-1.414Z" />
+                              </svg>
+                            ) : (
+                              <svg
+                                className="fill-indigo-500"
+                                width="32"
+                                height="32"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M21 15h-4v-4a1 1 0 0 0-2 0v4h-4a1 1 0 0 0 0 2h4v4a1 1 0 0 0 2 0v-4h4a1 1 0 0 0 0-2Z" />
+                              </svg>
+                            )}
+                          </div>
+                        </div>
+                      </button>
+                      {/* Add-on button #2 *****************************************************/}
+                      <button
+                        className={`w-full text-left py-3 px-4 border rounded ${
+                          stickOneWeek
+                            ? "border-green-300 ring-2 ring-green-300"
+                            : " border-red-200 ring-1 ring-red-200"
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setStickOneWeek(!stickOneWeek);
+                          handleAddOnSelection("1 week stick");
+                        }}
+                      >
+                        <div className="flex justify-between items-center">
+                          <div>
+                            <div className="text-sm text-gray-800 font-medium mb-1">
+                              Stick your post to stay on top for 1 week (+$299)
+                            </div>
+                            <div className="text-sm text-gray-500 italic">
+                              4x more views
+                            </div>
+                          </div>
+                          <div className="shrink-0 rounded-full border border-gray-200 ml-3">
+                            {stickOneWeek ? (
+                              <svg
+                                x-show="checked"
+                                className="fill-teal-500"
+                                width="32"
+                                height="32"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="m20.28 12.28-6.292 6.294-2.293-2.293a1 1 0 0 0-1.414 1.414l3 3a1 1 0 0 0 1.414 0l7-7a1 1 0 0 0-1.414-1.414Z" />
+                              </svg>
+                            ) : (
+                              <svg
+                                className="fill-indigo-500"
+                                width="32"
+                                height="32"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M21 15h-4v-4a1 1 0 0 0-2 0v4h-4a1 1 0 0 0 0 2h4v4a1 1 0 0 0 2 0v-4h4a1 1 0 0 0 0-2Z" />
+                              </svg>
+                            )}
+                          </div>
+                        </div>
+                      </button>
+                      {/* Add-on button #3 *****************************************************/}
+                      <button
+                        className={`w-full text-left py-3 px-4 border rounded ${
+                          highlight
+                            ? "border-green-300 ring-2 ring-green-300"
+                            : "border-red-200 ring-1 ring-red-200"
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setHighlight(!highlight);
+                          handleAddOnSelection("highlight");
+                        }}
+                      >
+                        <div className="flex justify-between items-center">
+                          <div>
+                            <div className="text-sm text-gray-800 font-medium mb-1">
+                              Highlight your post in yellow (+$49)
+                            </div>
+                            <div className="text-sm text-gray-500 italic">
+                              2x more views
+                            </div>
+                          </div>
+                          <div className="shrink-0 rounded-full border border-gray-200 ml-3">
+                            {highlight ? (
+                              <svg
+                                x-show="checked"
+                                className="fill-teal-500"
+                                width="32"
+                                height="32"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="m20.28 12.28-6.292 6.294-2.293-2.293a1 1 0 0 0-1.414 1.414l3 3a1 1 0 0 0 1.414 0l7-7a1 1 0 0 0-1.414-1.414Z" />
+                              </svg>
+                            ) : (
+                              <svg
+                                className="fill-indigo-500"
+                                width="32"
+                                height="32"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M21 15h-4v-4a1 1 0 0 0-2 0v4h-4a1 1 0 0 0 0 2h4v4a1 1 0 0 0 2 0v-4h4a1 1 0 0 0 0-2Z" />
+                              </svg>
+                            )}
+                          </div>
+                        </div>
+                      </button>
+                      {/* END OF button #3 *****************************************************/}
+                    </div>
+                    <div className="mt-6">
+                      <button
+                        className="btn w-full text-white bg-myred hover:bg-red-600 shadow-sm"
+                        type="submit"
+                        // onClick={checkout}
+                      >
+                        Pay Now - ${totalCost}
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </form>
             </div>
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ dotenv.config();
 const app = express();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 app.use(express.static("public"));
+app.use("/webhook", bodyParser.raw({ type: "*/*" }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(
@@ -36,58 +37,11 @@ app.get("/ReadJob", (req, res) => {
 });
 
 // ----------------------------------------------------------------------------
-// Stripe checkout
-app.post("/checkout", async (req, res) => {
-  const { totalCost, quantity } = req.body;
-  console.log(totalCost, quantity);
 
-  // Search for an existing price with the same unit amount and currency
-  const existingPrices = await stripe.prices.list({
-    active: true,
-    unit_amount: totalCost * 100,
-    currency: "cad",
-  });
-
-  // Checking if price_id already exist in my stripe dashboard. This is to avoid creating multiple price_ids
-  let price;
-  if (existingPrices.data.length > 0) {
-    price = existingPrices.data[0].id;
-    console.log("found a matching Price_ID");
-  } else {
-    // Otherwise, create a new price
-    const pricing = await stripe.existingPrices.create({
-      unit_amount: totalCost * 100,
-      currency: "cad",
-      product: "prod_NLDJAAdnnhGPKJ",
-    });
-    price = pricing.id;
-    console.log("created a new Price_ID");
-  }
-
-  // note that the names will have to be exactly "price" and "quantity" cos thats how stripe likes it.
-  let lineItems = [{ price: price, quantity: quantity }];
-
-  // creating a session with the line items
-  const session = await stripe.checkout.sessions.create({
-    line_items: lineItems,
-    mode: "payment",
-    success_url: "http://localhost:5173/success",
-    cancel_url: "http://localhost:5173/cancel",
-  });
-
-  //sending created session url to the frontend
-  res.send(
-    JSON.stringify({
-      url: session.url,
-    })
-  );
-});
-// ----------------------------------------------------------------------------
-//Push jobs to DB
-app.post("/CreateJob", upload.single("companyLogo"), (req, res) => {
+app.post("/CreateJob", upload.single("companyLogo"), async (req, res) => {
   try {
     // retrieve the data sent in the request body
-    const newJob = new postedjob({
+    const formData = {
       jobTitle: req.body.jobTitle,
       companyName: req.body.companyName,
       location: req.body.location,
@@ -113,30 +67,129 @@ app.post("/CreateJob", upload.single("companyLogo"), (req, res) => {
         : undefined,
 
       posted_date: new Date(),
-    });
+    };
     console.log("req.body: ", req.body);
-    console.log("req.file: ", req.file);
-    console.log("newJob: ", newJob);
+    // console.log("req.file: ", req.file);
+    console.log("totalCost is: ", req.body.totalCost);
 
     // check if a file was uploaded
     if (req.file) {
-      newJob.companyLogo = {
+      formData.companyLogo = {
         data: req.file.filename,
         contentType: req.file.mimetype,
       };
     }
+    // ******************************************************************************************
+    const { totalCost } = req.body;
+    console.log(totalCost);
 
-    postedjob.create(newJob, (err, savedJob) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(201).json(savedJob);
-      }
+    // Search for an existing price with the same unit amount and currency
+    const existingPrices = await stripe.prices.list({
+      active: true,
+      unit_amount: totalCost * 100,
+      currency: "cad",
     });
-  } catch (error) {
-    res.status(500).send(error);
+
+    // Checking if price_id already exist in my stripe dashboard. This is to avoid creating multiple price_ids
+    let price;
+    if (existingPrices.data.length > 0) {
+      price = existingPrices.data[0].id;
+      console.log("found a matching Price_ID");
+    } else {
+      // Otherwise, create a new price
+      const pricing = await stripe.existingPrices.create({
+        unit_amount: totalCost * 100,
+        currency: "cad",
+        product: "prod_NLDJAAdnnhGPKJ",
+      });
+      price = pricing.id;
+      console.log("created a new Price_ID");
+    }
+
+    // note that the names will have to be exactly "price" and "quantity" cos thats how stripe likes it.
+    let lineItems = [{ price: price, quantity: 1 }];
+
+    // creating a session with the line items
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: "payment",
+      success_url: "http://localhost:5173/success",
+      cancel_url: "http://localhost:5173/cancel",
+      metadata: {
+        // Pass form data to metadata for later retrieval
+        formData: JSON.stringify(formData),
+      },
+    });
+    // res.redirect(303, session.url);
+    res.send(
+      JSON.stringify({
+        url: session.url,
+      })
+    );
+
+    // ******************************************************************************************
+  } catch (err) {
+    console.log(err.message);
   }
 });
+
+//  -------------------------------------------------------------------------------------------------------------
+app.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  async (request, response) => {
+    // Verifying the event actually comes from stripe
+    const sig = request.headers["stripe-signature"];
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.body,
+        sig,
+        process.env.ENDPOINT_SECRET
+      );
+      console.log("webhook signature verified");
+    } catch (err) {
+      console.log(`webhook error: ${err.message}`);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+    // ******************************************************************************************
+    if (event.type === "checkout.session.completed") {
+      // Retrieve metadata to get form data
+      const session = event.data.object;
+      const metadata = session.metadata;
+      const formData = JSON.parse(metadata.formData);
+
+      const paymentIntentId = session.payment_intent;
+      const paymentIntent = await stripe.paymentIntents.retrieve(
+        paymentIntentId
+      );
+      console.log(paymentIntent);
+
+      if (paymentIntent.status === "succeeded") {
+        const newJob = new postedjob(formData);
+
+        postedjob.create(newJob, (err, savedJob) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Job created!");
+          }
+        });
+      } else {
+        // Payment failed, do not create job in database
+        console.error("Payment failed");
+      }
+      // // Return a success status code
+      // response.status(200).send("Webhook received");
+    } else {
+      console.log(`Unhandled event type: ${event.type}`);
+    }
+  }
+);
+
+// -------------------------------------------------------------------------------------------------------------
 
 app.listen(4000, () => {
   console.log("Server listening on port 4000");
